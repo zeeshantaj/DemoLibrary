e8553fbc3cb67358b77fc4546317ff1317d0cdee
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-08-13 18:51:59 +0500

    relaunch the app from background

--- app/src/main/java/com/example/dialog/WebConst.java
+++ app/src/main/java/com/example/dialog/WebConst.java
@@ -7,22 +7,12 @@ public class WebConst {
 
 //        // Development
         public String BASE_URL = "https://jumbilin-bkd-development.azurewebsites.net/api/";
-        public String MENU_BOARD_SOCKET = "https://jumbilinsocketdev-e8ce5f945195.herokuapp.com/";
-        public  String registerMenu = "https://jumbilin-bkd-development.azurewebsites.net/api/v3/MenuBoard";
-        public  String menuProducts = "https://jumbilin-bkd-development.azurewebsites.net/api/v2/MenuBoard?screenId=";
-        public  String RegisterUrlNew = "https://jumbilin-bkd-development.azurewebsites.net/api/v4/local/MenuBoard";
-        public static String internetStatus = "https://jumbilin-bkd-development.azurewebsites.net/api/DeviceManagement";
-//
 
 
     //    // Production
-//        public String BASE_URL = "http://jumbilinapis0343434343.azurewebsites.net/api/";
-//        public String MENU_BOARD_SOCKET = "https://jumbilinsocket.herokuapp.com/";
 //        public  String registerMenu = "http://jumbilinapis0343434343.azurewebsites.net/api/v3/MenuBoard";
-//        public  String menuProducts = "http://jumbilinapis0343434343.azurewebsites.net/api/v2/MenuBoard?screenId=";
-//        public  String RegisterUrlNew = "http://jumbilinapis0343434343.azurewebsites.net/api/v4/local/MenuBoard";
-//        public static String internetStatus = "http://jumbilinapis0343434343.azurewebsites.net/api/DeviceManagement";
-    public void checkForDevelopmentUrls() {
+//
+//    public void checkForDevelopmentUrls() {
         // for reminder to change url from development to production
 
 //        if (BuildConfig.CHECK_DEVELOPMENT_URLS){
@@ -39,5 +29,5 @@ public class WebConst {
 //            Log.d("MyApp","development");
 //        }
 
-    }
+    //}
 }
