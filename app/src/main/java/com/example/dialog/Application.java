cc82f3b7cd9da8781444a6944aa85215684c62b9
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-08-19 17:50:07 +0500

    task automation

--- app/src/main/java/com/example/dialog/Application.java
+++ app/src/main/java/com/example/dialog/Application.java
@@ -1,5 +1,6 @@
 package com.example.dialog;
 
+import android.content.pm.ApplicationInfo;
 import android.util.Log;
 
 import androidx.lifecycle.ProcessLifecycleOwner;
@@ -19,12 +20,26 @@ public class Application extends android.app.Application {
         AppLifeCycleObserver observer = new AppLifeCycleObserver(this);
         ProcessLifecycleOwner.get().getLifecycle().addObserver(observer);
 
-        if (!BuildConfig.DEBUG && WebConst.BASE_URL.contains("development")) {
-            throw new IllegalStateException("BASE_URL contains 'development' in release build!");
-        }
-        else {
-            Log.d("MyApp","else APK Released in mode "+BuildConfig.BUILD_TYPE);
-        }
+//        boolean IS_Debug = ( 0 != ( getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
+//
+//        if (IS_Debug){
+//            Log.d("MyApp","debug "+IS_Debug);
+//        }else {
+//            Log.d("MyApp","debug "+IS_Debug);
+//        }
+
+//        if (BuildConfig.IS_SIGNED) {
+//            Log.d("MyApplication", "This APK is signed.");
+//            if (WebConst.BASE_URL.contains("development")){
+//                throw new IllegalStateException("BASE_URL contains 'development' in release build!");
+//            }
+//            // Perform actions specific to the signed APK
+//            // Further checks or actions can be added here
+//        } else {
+//
+//            Log.d("MyApplication", "This APK is not signed.");
+//
+//        }
 
     }
 }
--- app/src/main/java/com/example/dialog/WebConst.java
+++ app/src/main/java/com/example/dialog/WebConst.java
@@ -5,30 +5,11 @@ import com.example.dialog.BuildConfig;
 
 public class WebConst {
 
-//        // Development
-        public static String BASE_URL = "https://jumbilin-bkd-development.azurewebsites.net/api/";
+//      Development
+        public static String BASE_URL = "https://jumbilin-bkd-development.azurewebsites.net/api/BuildConfig.";
 
-        //public static final String BASE_URL = BuildConfig.BASE_URL;
 
-    //    // Production
+//        Production
 //        public  String registerMenu = "http://jumbilinapis0343434343.azurewebsites.net/api/v3/MenuBoard";
-//
-//    public void checkForDevelopmentUrls() {
-        // for reminder to change url from development to production
 
-//        if (BuildConfig.CHECK_DEVELOPMENT_URLS){
-//            Log.d("MyApp","development");
-//            if (BASE_URL.contains("development") ||
-//                    MENU_BOARD_SOCKET.contains("development") ||
-//                    registerMenu.contains("development") ||
-//                    menuProducts.contains("development") ||
-//                    RegisterUrlNew.contains("development") ||
-//                    internetStatus.contains("development")) {
-//                throw new IllegalStateException("You're using development URLs in a production build!");
-//            }
-//        }else {
-//            Log.d("MyApp","development");
-//        }
-
-    //}
 }
